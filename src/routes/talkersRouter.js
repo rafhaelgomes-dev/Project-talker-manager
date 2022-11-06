da0406e121672abcb89b4d4fcecb56211931eda7
unknown <rafhaelgomes.dev>
2022-11-06 20:53:33 -0300

    ajuste lint

--- src/routes/talkersRouter.js
+++ src/routes/talkersRouter.js
@@ -3,14 +3,13 @@ const fsUtils = require('../utils/fsUtils');
 
 const router = express.Router();
 
-router.get('/',  async (req, res) => {
+router.get('/', async(req, res) => {
   const talker = await fsUtils.returnAllToSpeakers();
   if (talker.length === 0) {
     res.status(200).send([]);
   }
   res.status(200).json(talker);
-})
-
+});
 
 router.get('/:id',  async (req, res) => {
   const { id } = req.params;
